85fb68dc6e6b5fb709f1caf3f3c7d04e8972bd56
Aaron Saksik <aaron.saksik@ynov.com>
2020-05-10 20:16:31 +0200

    resp

--- ApiCovid.py
+++ ApiCovid.py
@@ -7,48 +7,38 @@ app = Flask(__name__)
 
 @app.route("/")
 def covidInit():
-   COVID_API_URL_WORLD = "https://api.covid19api.com/world/total"
-   responseWorld = requests.get(COVID_API_URL_WORLD)
-   contentWorld = json.loads(responseWorld.content.decode('utf-8'))
-   confirme = contentWorld['TotalConfirmed']
-   death = contentWorld['TotalDeaths']
-   recover = contentWorld['TotalRecovered']
-   COVID_API_URL_PAYS = "https://api.covid19api.com/summary"
-   responsePays = requests.get(COVID_API_URL_PAYS)
-   contentPays = json.loads(responsePays.content.decode('utf-8'))
-   countries = contentPays["Countries"]
-   return render_template('ApiCovid.html', countries=countries, confirme=confirme, death=death, recover=recover)
-
+    COVID_API_URL_WORLD = "https://api.covid19api.com/world/total"
+    responseWorld = requests.get(COVID_API_URL_WORLD)
+    contentWorld = json.loads(responseWorld.content.decode('utf-8'))
+    confirme = contentWorld['TotalConfirmed']
+    death = contentWorld['TotalDeaths']
+    recover = contentWorld['TotalRecovered']
+    COVID_API_URL_PAYS = "https://api.covid19api.com/summary"
+    responsePays = requests.get(COVID_API_URL_PAYS)
+    contentPays = json.loads(responsePays.content.decode('utf-8'))
+    countries = contentPays["Countries"]
+    return render_template('ApiCovid.html', countries=countries, confirme=confirme, death=death, recover=recover)
 
 
 @app.route("/",  methods=['POST'])
 def apiCovid():
-   COVID_API_URL_WORLD = "https://api.covid19api.com/world/total"
-   responseWorld = requests.get(COVID_API_URL_WORLD)
-   contentWorld = json.loads(responseWorld.content.decode('utf-8'))
-   confirme = contentWorld['TotalConfirmed']
-   death = contentWorld['TotalDeaths']
-   recover = contentWorld['TotalRecovered']
-   COVID_API_URL_PAYS = "https://api.covid19api.com/summary"
-   responsePays = requests.get(COVID_API_URL_PAYS)
-   contentPays = json.loads(responsePays.content.decode('utf-8'))
-   countries = contentPays["Countries"]
-   pays = request.form['pays']
-   date = request.form['date'] + "T00:00:00Z"
-   COVID_API_URL = "https://api.covid19api.com/live/country/{}/status/confirmed/date/{}".format(
+    COVID_API_URL_PAYS = "https://api.covid19api.com/summary"
+    responsePays = requests.get(COVID_API_URL_PAYS)
+    contentPays = json.loads(responsePays.content.decode('utf-8'))
+    countries = contentPays["Countries"]
+    glob = contentPays["Global"]
+    confirme = glob['TotalConfirmed']
+    death = glob['TotalDeaths']
+    recover = glob['TotalRecovered']
+
+    pays = request.form['pays']
+    date = request.form['date'] + "T00:00:00Z"
+    COVID_API_URL = "https://api.covid19api.com/live/country/{}/status/confirmed/date/{}".format(
         pays, date)
-   response = requests.get(COVID_API_URL)
-   content = json.loads(response.content.decode('utf-8'))
-   dataMin = []
-   dataMax = []
-   for i in content:
-      paysTest = i['Country']
-      dataMin.append(paysTest.lower())
-      dataMax.append(paysTest)
-   if pays in dataMin or pays in dataMax :
-        return render_template('ApiCovid.html', content=content, pays=pays, countries=countries, confirme=confirme, death=death, recover=recover)
-   else:
-        return render_template("errors/500.html"), 500
+    response = requests.get(COVID_API_URL)
+    content = json.loads(response.content.decode('utf-8'))
+    return render_template('ApiCovid.html', content=content, pays=pays, countries=countries, confirme=confirme, death=death, recover=recover)
+   
 
 @app.errorhandler(404)
 def page_not_found(error) :
